3279abfa527b07a248af4f9f77e99b8c1311d34b
newbie-dev-freyl <softwaretransjp@gmail.com>
2024-03-17 08:50:25 +0800

    completed javascript functionality for nav push menu

--- src/scripts/header-nav.js
+++ src/scripts/header-nav.js
@@ -1,40 +1,75 @@
-let windowWidth = document.documentElement.getBoundingClientRect().width;
 const headerNavToggle = document.querySelector('.header-toggle');
+const headerNavLinkAll = document.querySelectorAll('.header-navmenu a');
+const headerNavMenuAll = document.querySelectorAll('[toggle-menu] + ul');
 
 
-function fn_resize(size) {
+
+
+function fn_resize(size) {//function
+
+    //remove all toggled attribute on window resize
     headerNavToggle.removeAttribute('toggled');
+    headerNavMenuAll.forEach(menu => {
+        menu.removeAttribute('toggled');
+    });//remove all toggled attribute on window resize
 
-    if (windowWidth < size) {//if
+    if (document.documentElement.getBoundingClientRect().width > size) {//if
+        console.log('desktop mode')
 
-        headerNavToggle.onclick = () => {
+        //disable toggle menu click on desktop mode
+        headerNavLinkAll.forEach(link => {
+            link.onclick = function() {
+                if (link.hasAttribute('toggle-menu')) {
+                    link.nextElementSibling.removeAttribute('toggled');
+                }
+            } 
+        })//disable toggle menu click on desktop mode
+
+    } else {    
+        console.log('tablet mode')
+
+         //close all submenu on header toggle click
+        headerNavToggle.onclick = function() {
             headerNavToggle.toggleAttribute('toggled');
-        }
+            headerNavMenuAll.forEach(menu => {
+                menu.removeAttribute('toggled');
+            })
+        }//close all submenu on header toggle click
 
-        
-    }//if 
-}
+        headerNavLinkAll.forEach(link => {
+            link.onclick = function() {
+                if (link.hasAttribute('toggle-menu')) {
+                    link.nextElementSibling.toggleAttribute('toggled');
+                }
+
+                if (link.hasAttribute('toggle-back')) {
+                    link.parentElement.parentElement.toggleAttribute('toggled');
+                }
+            }   
+        })
+
+    }//if else
+
+}//function
 
 let resizeTimer;
-function fn_disable_animation() {
+function fn_disable_animation() {//function
     
     document.documentElement.setAttribute('disabled-animation', '');
     clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
+    resizeTimer = setTimeout(function() {
         document.documentElement.removeAttribute('disabled-animation');
     }, 100);
 
-}
+}//function
 
-window.onresize = () => {
+window.onresize = function() {
     fn_disable_animation();
-
-    
-    fn_resize(843.2);
+    fn_resize(843.20);
     
 }
 
-window.onload = () => {
-    fn_resize(843.2);
+window.onload = function() {
+    fn_resize(843.20);
 }
 
